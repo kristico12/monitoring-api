26b46732489fbf1cf829e6e2f694e801737eb089
Jesús Ricardo <kristico12@gmail.com>
2018-03-23 13:54:37 -0500

    new user and update user auth update!

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,58 @@
 const firebase = require('../adminfirebase');
 
-function createUser(req,res) {
-  firebase.admAuth.createUser({
-    uid:req.body.aut.identificationCard,    
-    email:req.body.aut.email,
-    password: req.body.aut.password,
-  })
-  .then((data) => {
-    res.status(200).json(data.uid);
-  })
-  .catch((error) => {
-    res.status(400).json(error)
-  })
+function createUser(req, res) {
+  const object = {
+    email: req.body.email,
+    password: req.body.password,
+  }
+  firebase.admAuth.createUser(object)
+    .then((data) => {
+      res.json({ success: true, data: data.uid });
+    })
+    .catch((error) => {
+      res.json({ success: false, data: error });
+    })
+}
+function getUser(req, res) {
+  const id = req.params.id;
+  firebase.admAuth.getUser(id)
+    .then((data) => {
+      res.status(200).json({ status: '200', data: data.toJSON().email });
+    })
+    .catch(error => {
+      res.status(400).json({ status: '400', data: error });
+    })
+}
+function updateUser(req, res) {
+  const id = req.params.id;
+  const object = {
+    email: req.body.email
+  }
+  if (req.body.password.length > 0) {
+    object.password = req.body.password
+  }  
+  firebase.admAuth.updateUser(id, object)
+    .then(() => {
+      res.json({ success: true });
+    })
+    .catch(error => {
+      res.json({ data: error, success: false });
+    })
+}
+function deleteUser(req, res) {
+  const id = req.params.id;
+  firebase.admAuth.deleteUser(id)
+    .then(() => {
+      res.status(200).json({ status: '200' });
+    })
+    .catch(error => {
+      res.status(400).json({ status: '400' });
+    })
 }
 
 module.exports = {
-  createUser
+  createUser,
+  getUser,
+  updateUser,
+  deleteUser
 }
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 'use strict'
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const express = require('express'); //incluir express
+const app = express(); //utilizar express
+const bodyParser = require('body-parser'); //incluir para que solo se vea el json
+const cors = require("cors") //incluir para que reciba llamadas externas
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
+app.use(cors()) //llamadas externas
+app.use(bodyParser.json()) //solo veamos el Json
+app.use(bodyParser.urlencoded({ //traducir
   extended: false
 }))
 
@@ -13,8 +15,12 @@ const auth = require('./controllers/auth');
 
 app.route('/api/auth')
   .post(auth.createUser)
+app.route('/api/auth/:id')
+  .get(auth.getUser)
+  .put(auth.updateUser)
+  .delete(auth.deleteUser)
 
-
+  
 app.listen(process.env.PORT || 5000, () => {
   console.log('puerto 5000')
 })
